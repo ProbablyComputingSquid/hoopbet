3503f9f1d9ab13b948a16b88090c0e9e70c6c06b
computingsquid <66024281+ProbablyComputingSquid@users.noreply.github.com>
2025-11-23 11:15:47 -0600

    fix registration!!

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // forward API calls to Express backend running on localhost:4000
+      '/register': 'http://localhost:4000',
+      '/users': 'http://localhost:4000',
+      '/users.json': 'http://localhost:4000',
+      '/markets': 'http://localhost:4000'
+    }
+  }
 })
